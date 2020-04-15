285cfdb1c8479f0e4a5326e29696991084d1c7bc
Yucheng Fang <664947387@qq.com>
2020-04-15 11:35:20 -0400

    update

--- sir_family/retrieve_data.py
+++ sir_family/retrieve_data.py
@@ -31,7 +31,8 @@ country_population_dict = {
     "New Jersey": 8882000,
     "Florida": 21300000,
     "Korea, South": 51470000,
-    "Washington": 7615000
+    "Washington": 7615000,
+    "Miami-Dade": 2717000
 }
 
 
@@ -86,6 +87,33 @@ def state_level(
     return result_df
 
 
+def city_level(
+    city, 
+    confirm_global=confirm_global, 
+    death_global=death_global
+    ):
+    remove_list = [
+        "UID", "iso2", "iso3", "code3", "FIPS", "Admin2", "Province_State",
+        "Country_Region", "Lat", "Long_", "Combined_Key"
+    ]
+    us_cols = confirm_us.columns
+    us_wanted_cols = [col for col in us_cols if col not in remove_list]
+
+    confirm = confirm_us.loc[confirm_us["Admin2"] == city,
+                             us_wanted_cols]
+    confirm = np.sum(confirm)
+    confirm = confirm[confirm > 0]
+    result_df = pd.DataFrame(confirm, columns=["confirm_count"])
+
+    death = death_us.loc[death_us["Admin2"] == city,
+                         us_wanted_cols]
+    death = np.sum(death)
+    result_df["death_count"] = death
+    result_df["death_rate"] = death / country_population_dict[city]
+    result_df.to_csv(output_path + city + ".csv")
+    return result_df
+
+
 country_level("United Kingdom")
 country_level("Italy")
 country_level("Spain")
@@ -97,3 +125,5 @@ state_level("Florida")
 state_level("New Jersey")
 state_level("New York")
 state_level("Washington")
+
+city_level("Miami-Dade")
